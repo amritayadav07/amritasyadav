757e0e84be7711e1c0686f124aa8e59bb8bdf360
amritayadav07 <61591304+amritayadav07@users.noreply.github.com>
2020-03-29 19:10:26 +0530

    Add files via upload

--- replace.py
+++ replace.py
@@ -1,9 +1,12 @@
-a=[]
+a = []
 n = int(input("enter the no:"))
 for x in range(n):
     print(x+1,"enter list")
     a.append(input())
-s = set(a)
-a = list(s)
-for x in a: 
- print(x)        
+N = 'rep'    
+has = set()
+for x in range(len(a)):
+    if a[x] in has:
+      a[x] = N
+    has.add(a[x]) 
+print(a)        
